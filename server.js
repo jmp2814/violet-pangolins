ef4ca4edd367da0faad144f6a00ed71400ebcc75
meriem0319 <meriem0319@gmail.com>
2022-09-10 12:55:22 -0400

    server file redone

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+const path = require("path");
 const express = require("express");
 const session = require("express-session");
+const { sequelize, sessionConfig } = require("./config/connection");
+const exphbs = require("express-handlebars");
+const controllers = require("./controllers");
+const helpers = require("./utils/helper.js");
 
+const PORT = process.env.PORT || 3005;
 const app = express();
+const hbs = exphbs.create({ helpers });
+
+app.use(session(sessionConfig));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(controllers);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
