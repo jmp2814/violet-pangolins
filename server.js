751c8c1950cb82f203fcaf4ef86d41531fd9f08c
meriem0319 <meriem0319@gmail.com>
2022-09-13 19:51:10 -0400

    server.js update

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const { sequelize, sessionConfig } = require("./config/connection");
-// const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const controllers = require("./controllers");
 //const helpers = require("./utils/helper.js");
